09c791ab7d4334bc90b0f72871e9e3d2efcc6824
ruhulfbr <ruhul11bd@gmail.com>
2023-08-27 13:21:56 +0600

    Has many relationship

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 
 class Blog extends Model
@@ -13,5 +14,10 @@ class Blog extends Model
     protected $table = 'blogs';
 //    protected $primaryKey = 'flight_id';
 //    public $incrementing = false;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
